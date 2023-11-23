1a05336ea333a535fbb2006557b6d845f97566d1
Soumya Sutar <soumsutar9@gmail.com>
2023-11-23 15:37:59 +0530

    ShapeAreaLib

---
+++ Area.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ShapeAreaLib
+{
+    public class Rectangle
+    {
+        public double CalculateArea(double length, double width)
+        {
+            return length * width;
+        }
+        public class Square
+        {
+            public double CalculateArea(double side)
+            {
+                return side * side;
+            }
+        }
+        public class circle
+        {
+            public double CalculateArea(double radius)
+            {
+                return Math.PI * radius * radius;
+            }
+        }
+    }
+    public class Triangle
+    {
+        public double CalculateArea(double baseLength, double height)
+        {
+            return 0.5 * baseLength * height;
+        }
+    }
+}
+
